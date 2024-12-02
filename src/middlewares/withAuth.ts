5e3284bf5377b0710175a1b70c6d6704f1a5cb03
neoproject18 <neoproject.info@gmail.com>
2024-12-02 20:50:29 +0700

    Login Multi Role

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -6,6 +6,8 @@ import {
   NextResponse,
 } from "next/server";
 
+const onlyAdmin = ["/admin"];
+
 export default function withAuth(
   middleware: NextMiddleware,
   requireAuth: string[] = []
@@ -23,6 +25,9 @@ export default function withAuth(
         const url = new URL("/auth/login", req.url);
         url.searchParams.set("callbackUrl", encodeURI(req.url));
         return NextResponse.redirect(url);
+      }
+      if (token.role !== "admin" && onlyAdmin.includes(pathname)) {
+        return NextResponse.redirect(new URL("/", req.url));
       }
     }
 
